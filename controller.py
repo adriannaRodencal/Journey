3a41457fba8a63f4161cf8010cb93458156b97d6
adriannaRodencal <adrianna.rodencal@llwarriors.org>
2020-04-23 13:09:07 -0500

    adding buttons

---
+++ controller.py
@@ -0,0 +1,158 @@
+import sys
+from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from time import sleep
+from view import Scene
+
+
+class MyApplication(QtWidgets.QMainWindow):
+    def __init__(self, app):
+        # MainWindow is, as you might expect, the main window
+        # of an application. It supports menus, status bars,
+        # toolbars and probably other stuff.
+        #
+        # Call __init__ for the parent class to initialize things.
+        super(MyApplication, self).__init__()
+
+        #
+        # Keep a reference to the app so we can explicitly call self.app.processEvents().
+        # You won't have to worry about this for a while. You'll know when you need it.
+        #
+        self.app = app
+
+        #
+        # Setup the main display window.
+
+
+        self.setup_window()
+        #
+        # Initialize the widget that will act as the display.
+        #
+        
+        self.display = Scene()
+        self.setCentralWidget(self.display)
+        self.display.show()
+
+
+        #
+        # Create actions, menus, toolbars and statusbar
+        #
+        self.create_actions()
+        self.create_menus()
+        self.create_tool_bars()
+        self.create_status_bar()
+
+        # self.event = MyLabel()
+        # self.event.show()
+
+        #
+        # This example only has one item in the main window. If you write
+        # a program where you have multiple items in the main window then
+        # you can control the layout of those items. Layout here means
+        # the relationship between the items on the screen. This can
+        # become complicated as users resize things, so "layouts" really
+        # help with that.
+        #
+        # There are: Vertical Box Layouts (QVBoxLayout), Horizontal Box
+        # Layouts (QHBoxLayout) and Grid Layouts (QGridLayout).
+        #
+        # Try it without these lines commented out. It is a subtle difference.
+        # See the layouts examples for, well, examples.
+        #
+        #mainLayout = QtWidgets.QVBoxLayout()
+        #mainLayout.addWidget(self.display)
+        #self.setLayout(mainLayout)
+
+    def setup_window(self):
+        """
+        Just putting a bunch of loosely related window setup stuff together here. This could
+        have gone in __init__(), but it was getting long.
+        """
+        #
+        # Starting size of window. I don't think this is required.
+        #
+        xSize = 1400
+        ySize = 800
+        self.setFixedSize(QSize(xSize, ySize))
+
+        #
+        # Starting coordinates of the window. This centers it on the desktop. Optional.
+        #
+        desktop = QtWidgets.QDesktopWidget().screenGeometry()
+        myWindow = self.geometry()
+        xLocation = (desktop.width() - myWindow.width()) / 2
+        yLocation = (desktop.height() - myWindow.height()) / 2
+        self.move(xLocation, yLocation)
+
+        #
+        # Misc window settings that you can use.
+        #
+        self.setWindowTitle("Treekthin ota ta Churi")
+        self.setWindowIcon(QtGui.QIcon('./icons/book.png'))
+        self.statusBar().showMessage('Opening')
+        self.show()
+
+    def create_actions(self):
+        """
+        Setup an action that can be associated with menus, buttons, shortcuts and taskbars.
+        Anything action that is initiated by interacting with the user interface (as opposed
+        to clicking directly in a window is setup here.
+        """
+        #
+        # Root is where the application exists in the directory structure.
+        #
+        root = QtCore.QFileInfo(__file__).absolutePath()
+
+        self.exitAction = QtWidgets.QAction("E&xit", self,
+                                            shortcut="Ctrl+Q",
+                                            statusTip="Exit the application",
+                                            triggered=self.quit)
+
+        self.aboutAction = QtWidgets.QAction("&About", self,
+                                             statusTip="More information about the program",
+                                             triggered=self.about)
+
+    def create_menus(self):
+        """Create a menubar and add a menu and an action."""
+
+        self.addAction(self.exitAction)
+
+        self.helpMenu = self.menuBar().addMenu("&Help")
+        self.helpMenu.addAction(self.aboutAction)
+
+    def create_tool_bars(self):
+        """Create a toolbar and add an action to it."""
+
+        # self.fileToolBar = self.addToolBar("File")
+        # self.fileToolBar.addAction(self.newAction)
+        # self.fileToolBar.addAction(self.openAction)
+        # self.fileToolBar.addAction(self.saveAction)
+
+
+    def create_status_bar(self):
+        self.statusBar().showMessage("Opening")
+        #
+        # You can also add widgets to the statusBar
+        #
+        # self.progressBar = QtWidgets.QProgressBar(self.statusBar())
+        # self.progressBar.hide()
+        # self.statusBar().addPermanentWidget(self.progressBar)
+
+    def quit(self):
+        self.close()
+
+    def about(self):
+        QtWidgets.QMessageBox.about(self, "Treekthin ota at Churi",
+                                    "On this journey you need to find your friend "
+                                    "who has been missing up in the mountains."
+                                    "Choose your choices wisely because one small "
+                                    "error could ruin it.")
+
+
+    def mousePresEvent(self, event):
+        print("click (display)")
+        x = event.pos().x()
+        y = event.pos().y()
+        print(x, y)
---
+++ view.py
@@ -0,0 +1,74 @@
+import model
+import sys, random
+from PyQt5 import QtGui, QtCore, QtWidgets, QtMultimedia
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+
+
+class Scene(QtWidgets.QWidget):
+
+    def __init__(self, parent=None, frame='riverChoose', possiblePaths=2):
+        super(Scene, self).__init__(parent)
+        
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.update)
+        self.timer.start(100)
+        self.frame = frame
+
+        self.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.create_buttons()
+
+
+    def paintEvent(self, event):
+        """
+        By magic, this event occasionally gets called. Maybe on self.update()? Certainly on
+        a window resize.
+        """
+        painter = QtGui.QPainter(self)
+        rectangle = self.contentsRect()
+
+        self.background = QtGui.QPixmap()
+        root = QtCore.QFileInfo(__file__).absolutePath()
+        self.background.load(root + f'/grahics/{self.frame}.jpg')
+
+
+        painter.drawPixmap(rectangle, self.background, rectangle)
+        painter.drawText(100, 100, "Hello")
+
+
+    def keyPressEvent(self, event):
+        """
+        You could, of course, do more interesting things than print here.
+        :param event:
+        :return:
+        """
+        if event.key() in [QtCore.Qt.Key_Right, QtCore.Qt.Key_Up]:
+            print('up')
+        elif event.key() in [QtCore.Qt.Key_Left, QtCore.Qt.Key_Down]:
+            print('down')
+        elif event.key() in [QtCore.Qt.Key_Enter, QtCore.Qt.Key_Return, QtCore.Qt.Key_Space]:
+            print('select')
+            self.update()
+
+    def wheelEvent(self, event):
+        """should work a lot like keypress..."""
+        if event.angleDelta().y() > 0:
+            print('up')
+        else:
+            print('down')
+
+    def create_buttons(self):
+        button1 = QPushButton('PyQt5 button', self)
+        button1.setToolTip('This is an example button')
+        button1.move(250, 625)
+        button1.clicked.connect(self.on_click)
+
+        button2 = QPushButton('This is random', self)
+        button2.setToolTip('This is an example button')
+        button2.move(1025, 550)
+        button2.clicked.connect(self.on_click)
+
+    def on_click(self):
+        print('PyQt5 button click')
