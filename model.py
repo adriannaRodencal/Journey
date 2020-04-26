616a233ae94e253f53c7b5bf78d65c8fe2202d64
Janna4 <janna.pink@icloud.com>
2020-04-26 18:41:30 -0500

    Added some doc strings and made the buttons display new backgrounds

--- model.py
+++ model.py
@@ -1,7 +1,20 @@
 import random
 import copy
+import view
 
 class Model(object):
 
     def __init__(self):
-        pass
+        self.__currentScene = None
+
+    def next_scene(self, theScene, newScene):
+        """
+        Send view the information needed for making a new scene
+        :param theScene: this is from the scene class that view is running
+        :param newScene: this is the name of the new scenes photo
+        :return: None
+        """
+        #
+        # This seems weird, but it appears to work
+        #
+        theScene.next_scene(newScene)
