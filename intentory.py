1fee65b7b42215320894c9796e32a97a01d0e343
adriannaRodencal <adrianna.rodencal@llwarriors.org>
2020-05-11 15:05:11 -0500

    Cordinate Updates

--- intentory.py
+++ intentory.py
@@ -6,18 +6,22 @@ from PyQt5.QtWidgets import *
 from time import sleep
 from view import Scene
 
-class Inventory(QtWidgets.QWidget):
+class Inventory(QWidget):
 
     def __init__(self, parent=None):
         super(Inventory, self).__init__(parent)
 
         self.setup_window()
+        self.create_buttons()
 
 
     def setup_window(self):
         xSize = 1400
         ySize = 800
         self.setFixedSize(QSize(xSize, ySize))
+        painter = QtGui.QPainter(self)
+        rectangle = self.contentsRect()
+        self.background = QtGui.QPixmap()
 
         #
         # Starting coordinates of the window. This centers it on the desktop. Optional.
@@ -33,5 +37,13 @@ class Inventory(QtWidgets.QWidget):
         #
         self.setWindowTitle("Inventory")
         self.setWindowIcon(QtGui.QIcon('./icons/book.png'))
-        self.statusBar().showMessage('Inventory')
+        painter.drawPixmap(rectangle, self.background , rectangle)
+        painter.drawText(100, 100, "Hello")
         self.show()
+
+    def create_buttons(self):
+        self.button = QtWidgets.QPushButton(' ', self)
+        # self.button.clicked.connect(self.handleButton)
+        self.button.setIcon(QtGui.QIcon('myImage.jpg'))
+        self.button.setIconSize(QtCore.QSize(24, 24))
+        self.button.move(200, 500)
